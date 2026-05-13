2d4468f572bd47eb6872e1ba0fa50bf8ed5fe16a
Ubuntu <aimashi@test.slu5tmrdvtquxmykwecsauv4lg.zrhx.internal.cloudapp.net>
2026-05-13 17:00:21 +0000

    fix: Kotlin 2.3.21 to match LiteRT-LM 0.11.0 metadata (2.3.0)
    LiteRT-LM 0.11.0 was compiled with Kotlin 2.3.0 metadata.
    Previous Kotlin 2.1.0 couldn't read 2.3.0 metadata.

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,6 @@
 // Top-level build file. All plugin versions are declared here and applied per-module.
 plugins {
     id("com.android.application") version "8.7.3" apply false
-    id("org.jetbrains.kotlin.android") version "2.1.0" apply false
-    id("org.jetbrains.kotlin.plugin.compose") version "2.1.0" apply false
+    id("org.jetbrains.kotlin.android") version "2.3.21" apply false
+    id("org.jetbrains.kotlin.plugin.compose") version "2.3.21" apply false
 }
